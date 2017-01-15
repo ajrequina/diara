8088d97618e380ff1b6d703e11e2a210f5752d0b
Arjemariel Requina <ajrequina1@gmail.com>
2017-01-15 21:46:34 +0800

    team threads and teams js are cleaned

--- static/javascripts/subcontrollers/teamthreads.js
+++ static/javascripts/subcontrollers/teamthreads.js
@@ -1,14 +1,4 @@
-﻿/*==========================================================
-    Author      : Ranjithprabhu K
-    Date Created: 13 Jan 2016
-    Description : Controller to handle Experience page
-    Change Log
-    s.no      date    author     description     
-
-
- ===========================================================*/
-
-app.controller("TeamThreadsController", ['$rootScope', '$scope', '$state', '$location','Flash',
+﻿app.controller("TeamThreadsController", ['$rootScope', '$scope', '$state', '$location','Flash',
 function ($rootScope, $scope, $state, $location,Flash) {
     var ctrl = this;
 
